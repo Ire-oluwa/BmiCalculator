633339106489032d115b525d55c9abd86325ea1c
Orodiji Ireoluwa <orodiji.ireoluwa@gmail.com>
2022-04-07 15:25:08 +0100

    ...

---
+++ lib/first_row_cards.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class FirstRowCardContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const FirstRowCardContent({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: Colors.white,
+          size: 80.0,
+        ),
+        const SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/input_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+import 'first_row_cards.dart';
+import 'reusable_cards.dart';
+
+const Color inactiveCardColour = Color(0xFF111328);
+const Color activeCardColour = Color(0xFF1D1E33);
+const Color bottomContainerColour = Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female,
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  late Gender gender;
+
+  @override
+  void initState() {
+    gender = Gender.male;
+    gender = Gender.female;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Calculator'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        gender = Gender.male;
+                      });
+                    },
+                    colour: gender == Gender.male
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: const FirstRowCardContent(
+                      icon: Icons.male,
+                      label: 'MALE',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        gender = Gender.female;
+                      });
+                    },
+                    colour: gender == Gender.female
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: const FirstRowCardContent(
+                      icon: Icons.female,
+                      label: 'FEMALE',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: ReusableCard(
+              colour: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(
+                    colour: inactiveCardColour,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: inactiveCardColour,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColour,
+            margin: const EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 80.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
